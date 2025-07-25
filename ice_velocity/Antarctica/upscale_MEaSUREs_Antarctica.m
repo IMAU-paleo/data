0bcf430f6e291d6a988ad1cfd1f5fa80cb7e0ee0
TijnBerends <c.j.berends@uu.nl>
2025-07-25 10:24:08 +0200

    Improve NaNs in MEaSUREs
    Improve the way the upscaling script for the MEaSUREs Antarctica ice velocity product deals with NaNs. Also make sure they still end up as NaN in the upscaled files, so UFEMISM's inversion code can deal with those missing values properly.

--- ice_velocity/Antarctica/upscale_MEaSUREs_Antarctica.m
+++ ice_velocity/Antarctica/upscale_MEaSUREs_Antarctica.m
@@ -44,9 +44,8 @@ u_surf = padarray( u_surf, [n_extra_x_east, n_extra_y_north], NaN, 'post');
 v_surf = padarray( v_surf, [n_extra_x_west, n_extra_y_south], NaN, 'pre');
 v_surf = padarray( v_surf, [n_extra_x_east, n_extra_y_north], NaN, 'post');
 
-% Calculate uabs_surf, substitute NaN with -10000
+% Calculate absolute surface velocity
 uabs_surf = sqrt( u_surf.^2 + v_surf.^2);
-uabs_surf( isnan(uabs_surf)) = -10000;
 
 % Start with the 450m version
 write_to_upscaled_file( x, y, u_surf, v_surf, uabs_surf, filename)
@@ -56,37 +55,55 @@ for n_up = 1:5
 
   %% Upscale data
 
-  % Upscale x
-
-  if mod( x(1), x(3)-x(1)) == 0
-    % Start from x(1)
-    x          = [x(         1  ); 0.25 * x(         2:2:end-3  ) + 0.5 * x(         3:2:end-2  ) + 0.25 * x(         4:2:end-1  ); x(         end  )];
-    u_surf     = [u_surf(    1,:); 0.25 * u_surf(    2:2:end-3,:) + 0.5 * u_surf(    3:2:end-2,:) + 0.25 * u_surf(    4:2:end-1,:); u_surf(    end,:)];
-    v_surf     = [v_surf(    1,:); 0.25 * v_surf(    2:2:end-3,:) + 0.5 * v_surf(    3:2:end-2,:) + 0.25 * v_surf(    4:2:end-1,:); v_surf(    end,:)];
-  else
-    % Start from x(2)
-    x          = 0.25 * x(         1:2:end-2  ) + 0.5 * x(         2:2:end-1  ) + 0.25 * x(         3:2:end  );
-    u_surf     = 0.25 * u_surf(    1:2:end-2,:) + 0.5 * u_surf(    2:2:end-1,:) + 0.25 * u_surf(    3:2:end,:);
-    v_surf     = 0.25 * v_surf(    1:2:end-2,:) + 0.5 * v_surf(    2:2:end-1,:) + 0.25 * v_surf(    3:2:end,:);
+  x_new = 0.25 * x(1:2:end-2) + 0.5 * x(2:2:end-1) + 0.25 * x(3:2:end);
+  y_new = 0.25 * y(1:2:end-2) + 0.5 * y(2:2:end-1) + 0.25 * y(3:2:end);
+  
+  nx = length( x_new);
+  ny = length( y_new);
+  
+  u_new = zeros( nx,ny);
+  v_new = zeros( nx,ny);
+  
+  w = [
+    0.25 * 0.25, 0.50 * 0.25, 0.25 * 0.25;
+    0.25 * 0.50, 0.50 * 0.50, 0.25 * 0.50;
+    0.25 * 0.25, 0.50 * 0.25, 0.25 * 0.25];
+  
+  pprev = -Inf;
+  for i = 1: nx
+  
+    p = floor( 100 * (i-1) / (nx-1) );
+    if p>pprev
+      pprev = p;
+      disp(['Progress: ' num2str(p) '%...'])
+    end
+  
+    for j = 1: ny
+  
+      ibl = 2*i - 1;
+      ibu = 2*i + 1;
+  
+      jbl = 2*j - 1;
+      jbu = 2*j + 1;
+      
+      ub = u_surf( ibl:ibu, jbl:jbu);
+      m = ~isnan( ub);
+      u_new( i,j) = sum(ub(m) .* w(m)) / sum(w(m));
+      
+      vb = v_surf( ibl:ibu, jbl:jbu);
+      m = ~isnan( vb);
+      v_new( i,j) = sum(vb(m) .* w(m)) / sum(w(m));
+  
+    end
   end
 
-  % Upscale y
+  x      = x_new;
+  y      = y_new;
+  u_surf = u_new;
+  v_surf = v_new;
 
-  if mod( y(1), y(3)-y(1)) == 0
-    % Start from y(1)
-    y          = [y(           1); 0.25 * y(           2:2:end-3) + 0.5 * y(           3:2:end-2) + 0.25 * y(           4:2:end-1); y(           end)];
-    u_surf     = [u_surf(    :,1), 0.25 * u_surf(    :,2:2:end-3) + 0.5 * u_surf(    :,3:2:end-2) + 0.25 * u_surf(    :,4:2:end-1), u_surf(    :,end)];
-    v_surf     = [v_surf(    :,1), 0.25 * v_surf(    :,2:2:end-3) + 0.5 * v_surf(    :,3:2:end-2) + 0.25 * v_surf(    :,4:2:end-1), v_surf(    :,end)];
-  else
-    % Start from y(2)
-    y          = 0.25 * y(           1:2:end-2) + 0.5 * y(           2:2:end-1) + 0.25 * y(           3:2:end);
-    u_surf     = 0.25 * u_surf(    :,1:2:end-2) + 0.5 * u_surf(    :,2:2:end-1) + 0.25 * u_surf(    :,3:2:end);
-    v_surf     = 0.25 * v_surf(    :,1:2:end-2) + 0.5 * v_surf(    :,2:2:end-1) + 0.25 * v_surf(    :,3:2:end);
-  end
-
-  % Replace NaN in uabs_surf with -10000
+  % Calculate absolute surface velocity
   uabs_surf = sqrt( u_surf.^2 + v_surf.^2);
-  uabs_surf(isnan(uabs_surf)) = -10000;
 
   % Write to NetCDF
   write_to_upscaled_file( x, y, u_surf, v_surf, uabs_surf, filename);
